017767077e328f7d6fce125e5451cde27543edcc
Amel Awadelkarim <amelawadelkarim19@gmail.com>
2018-11-18 17:44:10 -0800

    svm attempt

--- nn-test/get_track_features.sql
+++ nn-test/get_track_features.sql
@@ -1,13 +1,15 @@
 SELECT
-	"trackId" as track_id,
-	danceability,
-	energy,
-	mode,
-	speechiness,
-	acousticness,
-	instrumentalness,
-	liveness,
-	valence,
-	tempo,
-	time_signature
-FROM "AudioFeatures"
+	af."trackId" as track_id,
+	af.danceability,
+	af.energy,
+	af.mode,
+	af.speechiness,
+	af.acousticness,
+	af.instrumentalness,
+	af.liveness,
+	af.valence,
+	af.tempo,
+	af.time_signature,
+	pt."playlistId" as playlist_id
+FROM "AudioFeatures" af
+INNER JOIN playlist_track pt ON pt."trackId" = af."trackId";
--- nn-test/pytorch_data_set.py
+++ nn-test/pytorch_data_set.py
@@ -1,18 +1,10 @@
+import models
 import numpy as np
 import psycopg2
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(1, 1)
-
-    def forward(self, x):
-        x = F.softmax(self.fc1(x))
-        return x
+# import torch.nn.functional as F
 
 def main():
     # read data from database
@@ -22,8 +14,14 @@ def main():
     cur.execute(open('get_track_features.sql', 'r').read())
     features = cur.fetchall()
     features_array = np.array([row for row in features])
-    unique_tracks = set(features_array[:,0])
-    track_to_int = {list(unique_tracks)[i]: i for i in range(len(list(unique_tracks)))}
+    tracks = features_array[:, 0]
+    X = features_array[:, 1:-1].astype(np.float)
+    Y = features_array[:, -1]
+    print(tracks, X, Y)
+    unique_tracks = set(tracks)
+    unique_playlists = set(Y)
+    playlist_to_int = {list(unique_playlists)[i]: i for i in range(len(list(unique_playlists)))}
+    # track_to_int = {list(unique_tracks)[i]: i for i in range(len(list(unique_tracks)))}
 
     cur.execute(open('get_track_genres.sql', 'r').read())
     genres = cur.fetchall()
@@ -36,33 +34,33 @@ def main():
             track_genres[row[0]] = set()
             track_genres[row[0]].add(row[1])
 
-    cur.execute(open('get_playlist_tracks.sql', 'r').read())
-    playlists = cur.fetchall()
-    playlist_array = np.array([row for row in playlists])
-    unique_playlists = set(playlist_array[:][0])
-    playlist_to_int = {list(unique_playlists)[i]: i for i in range(len(list(unique_playlists)))}
+    # cur.execute(open('get_playlist_tracks.sql', 'r').read())
+    # playlists = cur.fetchall()
+    # playlist_array = np.array([row for row in playlists])
 
-    track_to_playlist = {}
-    for row in playlist_array:
-        if row[1] in track_to_playlist.keys():
-            track_to_playlist[row[1]].add(row[0])
-        else:
-            track_to_playlist[row[1]] = set()
-            track_to_playlist[row[1]].add(row[0])
 
-    net = Net()
+    track_to_playlist = {row[1]: row[0] for row in playlist_array}
+    # for row in playlist_array:
+        # if row[1] in track_to_playlist.keys():
+        #     track_to_playlist[row[1]].add(row[0])
+        # else:
+        #     track_to_playlist[row[1]] = set()
+        #     track_to_playlist[row[1]].add(row[0])
+
+    net = models.Net()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     for epoch in range(2):  # loop over the dataset multiple times
         running_loss = 0.0
         # for i, data in enumerate(trainloader, 0):
-        for sample in features_array:
+        m,n = X.shape
+        for i in range(m):
             # get the inputs
             # inputs, labels = data
 
             # zero the parameter gradients
-            input = torch.from_numpy(sample[1:].astype(np.float))
+            input = torch.from_numpy(X[i,:])
             # forward + backward + optimize
             output = net(input.float())
             # loss = criterion(output, target)
@@ -71,7 +69,7 @@ def main():
             # optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
+            # running_loss += loss.item()
             # # if i % 2000 == 1999:    # print every 2000 mini-batches
             # print('[%d, %5d] loss: %.3f' %
             #           (epoch + 1, i + 1, running_loss / 2000))
