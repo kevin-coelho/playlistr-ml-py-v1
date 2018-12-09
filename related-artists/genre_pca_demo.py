c452a17dc37d19696ceb4656fa93683336846a3e
Kevin <kevin@kevincoelho.com>
2018-12-09 14:28:40 -0800

    genre pca demo

--- related-artists/genre_pca_demo.py
+++ related-artists/genre_pca_demo.py
@@ -16,11 +16,11 @@ import adjustText
 # OUTPUT DIR
 CWD = os.path.dirname(os.path.realpath(__file__))
 RESULT_DIR = os.path.join(CWD, 'output')
-CORPUS_FILE = os.path.join(RESULT_DIR, 'related_artist_glove_corpus.model')
-GLOVE_MODEL_FILE = os.path.join(RESULT_DIR, 'related_artist_glove.model')
+CORPUS_FILE = os.path.join(RESULT_DIR, 'related_genre_glove_corpus.model')
+GLOVE_MODEL_FILE = os.path.join(RESULT_DIR, 'related_genre_glove.model')
 
-parser = argparse.ArgumentParser(description='Related artists PCA demo')
-parser.add_argument('QUERY', action='store', default='', help='Demo PCA using this artist')
+parser = argparse.ArgumentParser(description='Related genres PCA demo')
+parser.add_argument('QUERY', action='store', default='', help='Demo PCA using this genre')
 parser.add_argument('--corpus', '-c', default=CORPUS_FILE, help='Specify corpus file to read')
 parser.add_argument('--glove', '-g', default=GLOVE_MODEL_FILE, help='Specify glove model file to read')
 args = parser.parse_args()
@@ -61,26 +61,24 @@ similar_data = matrix[[dictionary[item] for item in similar_labels]]
 similar_colors = [args.QUERY for elem in similar]
 
 # get a 2nd item randomly
-#second_name, second_idx = random.choice(list(dictionary.items()))
-second_name, second_idx = ('Tim McGraw', dictionary['Tim McGraw'])
+second_name, second_idx = ('country', dictionary['country'])
 while second_name == args.QUERY:
     second_name, second_idx = random.choice(list(dictionary.items()))
 second = matrix[second_idx]
-second_similar = glove.most_similar(second_name, number=30)[11:30:6]
+second_similar = glove.most_similar(second_name, number=4)
 second_similar_labels = [elem[0] for elem in second_similar]
 second_similar_data = matrix[[dictionary[item] for item in second_similar_labels]]
-second_similar_colors = ['Tim McGraw' for elem in similar]
+second_similar_colors = ['country' for elem in similar]
 
 # get a 3rd item randomly
-# third_name, third_idx = random.choice(list(dictionary.items()))
-third_name, third_idx = ('Bob Marley & The Wailers', dictionary['Bob Marley & The Wailers'])
+third_name, third_idx = ('salsa', dictionary['salsa'])
 while third_name == args.QUERY:
     third_name, third_idx = random.choice(list(dictionary.items()))
 third = matrix[third_idx]
-third_similar = glove.most_similar(third_name, number=9)[0:9:3]
+third_similar = glove.most_similar(third_name, number=4)
 third_similar_labels = [elem[0] for elem in third_similar]
 third_similar_data = matrix[[dictionary[item] for item in third_similar_labels]]
-third_similar_colors = ['Bob Marley' for elem in similar]
+third_similar_colors = ['salsa' for elem in similar]
 
 X = np.array([primary, second, third])
 for data in [similar_data, second_similar_data, third_similar_data]:
@@ -88,7 +86,7 @@ for data in [similar_data, second_similar_data, third_similar_data]:
 labels = np.array([args.QUERY, second_name, third_name])
 for data in [similar_labels, second_similar_labels, third_similar_labels]:
     labels = np.append(labels, data, axis=0)
-colors = np.array([args.QUERY, 'Tim McGraw', 'Bob Marley'])
+colors = np.array([args.QUERY, 'country', 'salsa'])
 for color_arr in [similar_colors, second_similar_colors, third_similar_colors]:
     colors = np.append(colors, color_arr, axis=0)
 
@@ -97,7 +95,7 @@ transformed = pca.fit_transform(X)
 dataset = pd.DataFrame({'X': transformed[:, 0], 'Y': transformed[:, 1], 'color': colors})
 
 ax = sns.lmplot(x='X', y='Y', hue='color', data=dataset, fit_reg=False, height=8, aspect=2.5, scatter_kws={'s': 300}, legend=False)
-plt.title('Related artists: {}, Tim McGraw, Bob Marley & The Wailers'.format(args.QUERY))
+plt.title('Related genres: {}, country, salsa'.format(args.QUERY))
 plt.xlabel('X')
 plt.ylabel('Y')
 texts = []
@@ -112,7 +110,7 @@ def label_point(ax):
 label_point(plt.gca())
 plt.legend(loc='center left')
 plt.subplots_adjust(right=1)
-plt.tight_layout(pad=1)
+plt.tight_layout(pad=2)
 adjustText.adjust_text(texts)
 plt.show()
 
